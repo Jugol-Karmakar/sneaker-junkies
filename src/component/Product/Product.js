a8da551973c5b38cc4e37436cd4daad6985f641e
JugolKarmakar <jugol.cse@gmail.com>
2022-03-26 12:37:49 +0600

    cart added commit

--- src/component/Product/Product.js
+++ src/component/Product/Product.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons";
 import "./Product.css";
 
 const Product = (props) => {
@@ -6,11 +8,18 @@ const Product = (props) => {
   return (
     <div className="product">
       <img src={image} alt="" />
-      <div>
-        <h3>{name}</h3>
-        <h4>{price}</h4>
+      <div className="product-info">
+        <h3 className="product-name">{name}</h3>
+        <h4>Price : {price}</h4>
         <p>Rating : {ratings}</p>
       </div>
+      <button className="cart-btn">
+        <p className="cart-text">Add To Cart</p>
+        <FontAwesomeIcon
+          className="icon"
+          icon={faShoppingCart}
+        ></FontAwesomeIcon>
+      </button>
     </div>
   );
 };
